7b0ad4e117e1f645644fbcfe9cfe7873240d9301
Pascal Birchler <hello@pascalbirchler.ch>
2014-12-06 16:07:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# oEmbed API #
+**Contributors:** swissspidy
+**Tags:** api, json, REST, rest-api, oembed, provider
+**Requires at least:** 3.0
+**Tested up to:** 4.1
+**Stable tag:** 0.0.1
+
+An oEmbed provider for WordPress using the WP-API plugin.
+
+## Description ##
+
+This plugins adds oEmbed support to WordPress using the WP-API and makes WordPress an oEmbed provider. It aims to be compliant with the JSON specification at [oEmbed.com](http://www.oembed.com).
+
+## Installation ##
+
+Make sure the [JSON REST API](https://wordpress.org/plugins/json-rest-api/) is activated prior to installing this plugin.
+
+1. Upload the plugin folder `oembed-api` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Step 3? There is no step 3!
+
+## Frequently Asked Questions ##
+
+### Is it really that easy to install this plugin? ###
+
+Yes!
+
+## Changelog ##
+
+### 0.0.1 ###
+* Initial release
---
+++ oembed-api.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Plugin Name:       oEmbed API
+ * Plugin URI:        https://github.com/swissspidy/oembed-api
+ * Description:       Adds oEmbed support to WordPress using the WP-API
+ * Version:           0.0.1
+ * Author:            Pascal Birchler
+ * Author URI:        http://pascalbirchler.com/
+ * Text Domain:       oembed-api
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ */
+
+/**
+ * Plugin Activation
+ */
+function oembed_api_acivation() {
+	if ( ! function_exists( 'get_json_url' ) ) {
+		exit( __( 'The oEmbed API plugin requires WP-API to be installed!', 'oembed-api' ) );
+	}
+
+	oembed_api_endpoint();
+	flush_rewrite_rules();
+}
+
+register_activation_hook( __FILE__, 'oembed_api_acivation' );
+
+/**
+ * Plugin Deactivation
+ */
+function oembed_api_deacivation() {
+	flush_rewrite_rules();
+}
+
+register_deactivation_hook( __FILE__, 'oembed_api_deacivation' );
+
+/**
+ * Add the rewrite endpoint that is used to output the data.
+ */
+function oembed_api_endpoint() {
+	add_rewrite_endpoint( 'embed', EP_PERMALINK );
+	do_action( 'oembed_api_add_endpoint' );
+}
+
+add_action( 'init', 'oembed_api_endpoint' );
+
+/**
+ * Add our oEmbed API endpoint.
+ *
+ * @param WP_JSON_Server $server
+ */
+function oembed_api_init( WP_JSON_Server $server ) {
+	include dirname( __FILE__ ) . '/includes/class-oembed-api.php';
+	new OEmbed_API( $server );
+
+	$format = apply_filters( 'oembed_api_url_format', get_home_url() . '/*' );
+	wp_oembed_add_provider( $format, get_json_url( null, 'oembed' ) );
+}
+
+add_action( 'wp_json_server_before_serve', 'oembed_api_init' );
+
+/**
+ * Add oEmbed discovery links to single talent & product pages
+ */
+function oembed_api_discovery_links() {
+	if ( is_singular() ) {
+		echo '<link rel="alternate" type="application/json+oembed" href="' . esc_url( get_json_url( null, 'oembed/?url=' . get_permalink() ) ) . '" />' . "\n";
+	}
+}
+
+add_action( 'wp_head', 'oembed_api_discovery_links' );
