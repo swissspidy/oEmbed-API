eca64355827981254c338aff568cae84f206bfcc
Pascal Birchler <hello@pascalbirchler.ch>
2015-07-20 08:58:00 +0200

    Apply coding standards

--- classes/frontend.php
+++ classes/frontend.php
@@ -56,66 +56,75 @@ class WP_API_oEmbed_Frontend {
 	 */
 	public function rest_oembed_output( $post ) {
 		$post_content = strip_tags( $post->post_content );
-		$words = str_word_count( $post_content );
+		$words        = str_word_count( $post_content );
 		if ( count( $words ) > 35 ) {
-			$more_words = count( $words ) - 30;
+			$more_words     = count( $words ) - 30;
 			$oembed_content = implode( ' ', array_slice( $words, 0, 30 ) );
 			$oembed_content .= sprintf( ' <span class="oembed-words>"(' . _n( '%d word', '%d words', $more_words ) . ')</span>', $more_words );
 		} else {
 			$oembed_content = $post_content;
 		}
-	?>
-<html>
-	<head>
-		<title><?php esc_html_e( $post->post_title ); ?></title>
-		<style type="text/css">
-			html, body {
-				padding: 0;
-				margin: 0;
-			}
-			body {
-				border: 1px solid #aaa;
-				padding: 20px;
-				font-family: sans-serif;
-			}
-			.oembed-header h1 {
-				font-size: 1.5em;
-			}
-			.oembed-content {
-				font-size: 0.9em;
-				color: #aaa;
-			}
-			.oembed-content .oembed-words {
-				color: #ccc;
-			}
-			.oembed-footer-site {
-				float: left;
-				color: #777;
-			}
-			.oembed-footer-site img {
-				height: 32px;
-				width: 32px;
-			}
-			.oembed-footer-comments {
-				float: right;
-			}
-			.oembed-footer-comments .comments::after {
-				content: '';
-				display: inline-block;
-				background: url('<?php echo admin_url( 'images/comment-grey-bubble.png' ); ?>') 3px 5px no-repeat;
-				height: 16px;
-				width: 16px;
-			}
-		</style>
-	</head>
-	<body>
+		?>
+		<html>
+		<head>
+			<title><?php esc_html_e( $post->post_title ); ?></title>
+			<style type="text/css">
+				html, body {
+					padding: 0;
+					margin: 0;
+				}
+
+				body {
+					border: 1px solid #aaa;
+					padding: 20px;
+					font-family: sans-serif;
+				}
+
+				.oembed-header h1 {
+					font-size: 1.5em;
+				}
+
+				.oembed-content {
+					font-size: 0.9em;
+					color: #aaa;
+				}
+
+				.oembed-content .oembed-words {
+					color: #ccc;
+				}
+
+				.oembed-footer-site {
+					float: left;
+					color: #777;
+				}
+
+				.oembed-footer-site img {
+					height: 32px;
+					width: 32px;
+				}
+
+				.oembed-footer-comments {
+					float: right;
+				}
+
+				.oembed-footer-comments .comments::after {
+					content: '';
+					display: inline-block;
+					background: url('<?php echo admin_url( 'images/comment-grey-bubble.png' ); ?>') 3px 5px no-repeat;
+					height: 16px;
+					width: 16px;
+				}
+			</style>
+		</head>
+		<body>
 		<div class="oembed-header">
 			<h1><?php esc_html_e( $post->post_title ); ?></h1>
 		</div>
 		<div class="oembed-content"><?php echo $oembed_content; ?></div>
 		<div class="oembed-footer">
 			<div class="oembed-footer-site">
-				<img src="<?php echo esc_url( get_site_icon_url( null, 32, admin_url( 'images/w-logo-blue.png' ) ) ); ?>" />
+				<img
+					src="<?php echo esc_url( get_site_icon_url( null, 32, admin_url( 'images/w-logo-blue.png' ) ) ); ?>"/>
 				<?php bloginfo( 'name' ); ?>
 			</div>
 			<div class="oembed-footer-comments">
@@ -123,9 +132,9 @@ class WP_API_oEmbed_Frontend {
 				<span class="sharing">LOL</span>
 			</div>
 		</div>
-	</body>
-</html>
-	<?php
+		</body>
+		</html>
+		<?php
 	}
 
 	/**
@@ -134,6 +143,7 @@ class WP_API_oEmbed_Frontend {
 	 * @param string $html The unfiltered oEmbed HTML.
 	 * @param string $url  URL of the content to be embedded.
 	 * @param string $args Optional arguments, usually passed from a shortcode.
+	 *
 	 * @return string The filtered oEmbed HTML.
 	 */
 	public function filter_oembed_result( $html, $url, $args ) {
@@ -145,7 +155,7 @@ class WP_API_oEmbed_Frontend {
 			list( $providerurl, $regex ) = $data;
 
 			// Turn the asterisk-type provider URLs into regex
-			if ( !$regex ) {
+			if ( ! $regex ) {
 				$matchmask = '#' . str_replace( '___wildcard___', '(.+)', preg_quote( str_replace( '*', '___wildcard___', $matchmask ), '#' ) ) . '#i';
 				$matchmask = preg_replace( '|^#http\\\://|', '#https?\://', $matchmask );
 			}
@@ -158,13 +168,13 @@ class WP_API_oEmbed_Frontend {
 
 		$allowed_html = array(
 			'iframe' => array(
-				'src' => true,
-				'width' => true,
-				'height' => true,
-				'frameborder' => true,
-				'marginwidth' => true,
+				'src'          => true,
+				'width'        => true,
+				'height'       => true,
+				'frameborder'  => true,
+				'marginwidth'  => true,
 				'marginheight' => true,
-				'scrolling' => true,
+				'scrolling'    => true,
 			),
 		);
 
