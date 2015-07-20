a5aace7d190d557a2230e762980b1ebcc2026005
Gary Pendergast <gary@pento.net>
2015-07-20 13:57:23 +1000

    Enable oEmbed auto-discovery.

--- classes/plugin.php
+++ classes/plugin.php
@@ -58,6 +58,8 @@ class WP_API_oEmbed_Plugin extends WP_Stack_Plugin2 {
 		add_action( 'wp_head', array( $this->frontendClass, 'add_oembed_discovery_links' ) );
 		add_action( 'template_redirect', array( $this->frontendClass, 'template_redirect' ) );
 		add_action( 'rest_oembed_output', array( $this->frontendClass, 'rest_oembed_output' ) );
+
+		add_filter( 'embed_oembed_discover', '__return_true' );
 	}
 
 	/**
