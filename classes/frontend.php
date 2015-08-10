1781d07cd43081b94d50a302e020c1672882466f
Gary Pendergast <gary@pento.net>
2015-08-10 20:37:57 +1000

    Move the frontend JS to frontend.js

--- classes/frontend.php
+++ classes/frontend.php
@@ -54,30 +54,7 @@ class WP_API_oEmbed_Frontend {
 	public function add_host_js() {
 		?>
 		<script type="text/javascript">
-			function receiveEmbedMessage( e ) {
-				if ( 'height' == e.data.message ) {
-					var iframes = document.getElementsByTagName( 'iframe' );
-					for( var ii = 0; ii < iframes.length; ii++ ) {
-						if ( iframes[ ii ].getAttribute( 'data-password' ) == e.data.password ) {
-							var height = e.data.value;
-							if ( height > 600 ) {
-								height = 600;
-							} else if ( height < 100 ) {
-								height = 100;
-							}
-
-							iframes[ ii ].height = (height) + "px";
-						}
-					}
-				}
-			}
-
-			if ( window.addEventListener ) {
-				window.addEventListener( 'message', receiveEmbedMessage, false );
-			}
-			else if ( window.attachEvent ) {
-				window.attachEvent( 'message', receiveEmbedMessage );
-			}
+			<?php readfile( dirname( dirname( __FILE__ ) ) . "/scripts/frontend.js" ); ?>
 		</script>
 		<?php
 	}
