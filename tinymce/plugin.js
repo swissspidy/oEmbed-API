198718a579d4b140547b6dcfbc993c7a02ccc60e
Gary Pendergast <gary@pento.net>
2015-08-10 20:38:11 +1000

    Clean up the TinyMCE plugin

--- tinymce/plugin.js
+++ tinymce/plugin.js
@@ -1,38 +1,15 @@
 ( function( tinymce ) {
 	tinymce.PluginManager.add( 'autoembed', function( editor, url ) {
-		editor.on( 'preinit', function() {
-			var doc = editor.getDoc();
+		editor.on( 'init', function() {
+			var scriptId = editor.dom.uniqueId();
 
-			var jscript = "(function() { \
-					function receiveEmbedMessage ( e ) { \
-						if ( 'height' == e.data.message ) { \
-							var iframes = document.getElementsByTagName( 'iframe' ); \
-							for( var ii = 0; ii < iframes.length; ii++ ) { \
-								if ( iframes[ ii ].getAttribute( 'data-password' ) == e.data.password ) { \
-									var height = e.data.value; \
-									if ( height > 600 ) { \
-										height = 600; \
-									} else if ( height < 100 ) { \
-										height = 100; \
-									} \
-									iframes[ ii ].height = (height) + 'px'; \
-								} \
-							} \
-						} \
-					}; \
-					if ( window.addEventListener ) { \
-						window.addEventListener( 'message', receiveEmbedMessage, false ); \
-					} \
-					else if ( window.attachEvent ) { \
-						window.attachEvent( 'message', receiveEmbedMessage ); \
-					} \
-			})()";
+			var scriptElm = editor.dom.create( 'script', {
+				id: scriptId,
+				type: 'text/javascript',
+				src: url + '/../scripts/frontend.js'
+			} );
 
-			var script = doc.createElement( 'script' );
-			script.type = 'text/javascript';
-			script.appendChild( doc.createTextNode( jscript ) );
-
-			doc.getElementsByTagName( 'head' )[0].appendChild( script );
+			editor.getDoc().getElementsByTagName( 'head' )[0].appendChild( scriptElm );
 		} );
 	} );
 } )( window.tinymce );
