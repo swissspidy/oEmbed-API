5ca57b9de0d12b36bf3a1caee902b97d35ec03a0
Pascal Birchler <hello@pascalbirchler.ch>
2015-09-22 10:35:18 +0200

    Add `is_post_type_viewable` shim for unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,3 +17,23 @@ function _manually_load_oembed_api_plugin() {
 tests_add_filter( 'muplugins_loaded', '_manually_load_oembed_api_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
+
+/**
+ * Include functions that are in trunk unit testing suite, but not yet in all versions of WordPress core.
+ */
+if ( ! function_exists( 'is_post_type_viewable' ) ) {
+	/**
+	 * Determines whether a post type is considered "viewable".
+	 *
+	 * For built-in post types such as posts and pages, the 'public' value will be evaluated.
+	 * For all others, the 'publicly_queryable' value will be used.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @param object $post_type_object Post type object.
+	 * @return bool Whether the post type should be considered viewable.
+	 */
+	function is_post_type_viewable( $post_type_object ) {
+		return $post_type_object->publicly_queryable || ( $post_type_object->_builtin && $post_type_object->public );
+	}
+}
