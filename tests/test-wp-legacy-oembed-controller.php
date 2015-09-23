786cdbf1d83c7d36ef0734d44996c4bad5fc9109
John Blackbourn <johnbillion@gmail.com>
2015-09-23 12:15:03 +0100

    Tests for XML responses.

--- tests/test-wp-legacy-oembed-controller.php
+++ tests/test-wp-legacy-oembed-controller.php
@@ -166,7 +166,7 @@ class WP_Legacy_oEmbed_Test_Controller extends WP_UnitTestCase {
 		$request = array(
 			'url'	  => get_permalink( $post->ID ),
 			'format'   => 'xml',
-			'maxwidth' => 600,
+			'maxwidth' => 400,
 			'callback' => '',
 		);
 
@@ -174,8 +174,28 @@ class WP_Legacy_oEmbed_Test_Controller extends WP_UnitTestCase {
 
 		$data = $legacy_controller->dispatch( $request );
 
-		$xml = simplexml_load_string( $data );
-		$this->assertInstanceOf( 'SimpleXMLElement', $xml );
+		$data = simplexml_load_string( $data );
+		$this->assertInstanceOf( 'SimpleXMLElement', $data );
+
+		$data = (array) $data;
+
+		$this->assertArrayHasKey( 'version', $data );
+		$this->assertArrayHasKey( 'provider_name', $data );
+		$this->assertArrayHasKey( 'provider_url', $data );
+		$this->assertArrayHasKey( 'author_name', $data );
+		$this->assertArrayHasKey( 'author_url', $data );
+		$this->assertArrayHasKey( 'title', $data );
+		$this->assertArrayHasKey( 'type', $data );
+		$this->assertArrayHasKey( 'width', $data );
+
+		$this->assertEquals( '1.0', $data['version'] );
+		$this->assertEquals( get_bloginfo( 'name' ), $data['provider_name'] );
+		$this->assertEquals( get_home_url(), $data['provider_url'] );
+		$this->assertEquals( $user->display_name, $data['author_name'] );
+		$this->assertEquals( get_author_posts_url( $user->ID, $user->user_nicename ), $data['author_url'] );
+		$this->assertEquals( $post->post_title, $data['title'] );
+		$this->assertEquals( 'rich', $data['type'] );
+		$this->assertTrue( $data['width'] <= $request['maxwidth'] );
 	}
 
 	/**
